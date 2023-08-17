7e68059f2a87ef58c6195fd5cf373004ce871765
Shoaib <shoaibkhan133@gmail.com>
2023-08-17 15:58:39 +0530

    Aug 1, 8 PM

--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    func socialLogin() {
+        print("Worked on Social login and pushed on 8 PM")
+    }
 }
 
